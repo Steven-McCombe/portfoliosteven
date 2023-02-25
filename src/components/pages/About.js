b49c5ee9753e2cea19057a90adce6ee677fdd145
Steven-McCombe <smccombe93@gmail.com>
2023-02-24 23:21:11 -0500

    finished about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import "../styles/About.css"
+import backgroundImage from "../Assets/Images/Profile_Picture.jpg"
 
 export default function About() {
+  const backgroundStyles = {
+    backgroundImage: `url(${backgroundImage})`
+  };
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div id="hero" className="hero route bg-image" style={backgroundStyles}>
+    <div className="overlay-itro"></div>
+    <div className="hero-content display-table">
+      <div className="table-cell">
+        <div className="container">
+          <p className="display-6 color-d">Hello, world!</p>
+          <h1 className="hero-title mb-4">I'm Steven McCombe</h1>
+          <p className="hero-subtitle"><span>Full Stack Web Designer</span></p>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
