db16f2a8aea21cd53247e093a46fbbae2b0a6b87
Steven-McCombe <smccombe93@gmail.com>
2023-02-28 12:26:17 -0500

    added resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,38 +1,34 @@
-import React, {useState, useEffect } from 'react';
+import React from 'react';
 import Button from "react-bootstrap/Button";
 import { Container, Row } from "react-bootstrap";
-import pdf from "../assets/STEVEN_MCCOMBE_RESUME2023.pdf";
+import File from "../assets/STEVEN_MCCOMBE_RESUME2023.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-const resumeLink =
-  "https://raw.githubusercontent.com/Steven-McCombe/portfoliosteven/master/src/components/assets/STEVEN_MCCOMBE_RESUME2023.pdf";
+
+const Link =
+  "https://raw.githubusercontent.com/Steven-McCombe/portfoliosteven/main/src/components/assets/STEVEN_MCCOMBE_RESUME2023.pdf";
 
 export default function Resume() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
 
   return (
     <div>
-      <Container fluid className="resume-section">
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row className ="justify-content-center relative m-3">
           <Button
-            variant="primary"
-            href={pdf}
+            variant="light"
+            href={File}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
-            &nbsp;Download Resume
+            Download Resume
           </Button>
         </Row>
+      <Container fluid className=" m-3">
+        <Row >
+          <Document file={Link} className="d-flex justify-content-center">
+            <Page pageNumber={1} />
+          </Document>
+        </Row>
+
       </Container>
     </div>
   );
