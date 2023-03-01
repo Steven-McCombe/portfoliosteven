21df2918ea293f382695781f2e9a42d93c27e127
Steven-McCombe <smccombe93@gmail.com>
2023-03-01 18:58:42 -0500

    added manifest and updated document title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import react from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
+
     return (
         <div className = 'bg-dark'>
             <PortfolioContainer />
